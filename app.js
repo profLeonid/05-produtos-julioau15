deb2177f2422d0679de198bd15da1f43043ece91
JULIO AUGUSTO BARBOSA DA SILVA <25203694@dsn127m1242315.cfp127aluno.local>
2026-02-24 11:13:22 -0300

    adicionado novos inputs e adicionado tabela com os dados

--- app.js
+++ app.js
@@ -2,23 +2,52 @@
 
 function adicionarProduto(){
     const inputProduto = document.getElementById('input-produto')
+    const inputCodigo = document.getElementById('input-codigo')
+    const inputQuant = document.getElementById('input-quant')
     const produtosContainer = document.getElementById('produtos-container')
-    if(inputProduto.value == ''){
-        alert('erro');
-        inputProduto.focus()
-    }else{
-        let textProduto = document.createElement('span')
-        textProduto.textContent = inputProduto.value
-        textProduto.className = 'border-be-2 w-[100%] text-center'
 
-        produtosContainer.appendChild(textProduto)
+    if(inputProduto.value == '' || inputCodigo.value == '' || inputQuant.value == ''){
+        alert('erro');
+        inputCodigo.focus()
+    }else{
+        let textProduto = document.createElement('th')
+        let textCodigo = document.createElement('th')
+        let textQuant = document.createElement('th')
+        textProduto.textContent = inputProduto.value
+        textCodigo.textContent = inputCodigo.value
+        textQuant.textContent = inputQuant.value
+        
+        let tr = document.createElement('tr')
+        tr.className = 'flex justify-around border-be-2 p-1'
+        tr.appendChild(textCodigo)
+        tr.appendChild(textProduto)
+        tr.appendChild(textQuant)
+
+        produtosContainer.appendChild(tr)
         inputProduto.value = ''
-        inputProduto.focus()
+        inputCodigo.value = ''
+        inputQuant.value = ''
+        inputCodigo.focus()
     }
 }
 
 function limparLista(){
     const produtosContainer = document.getElementById('produtos-container')
     produtosContainer.innerText = ''
-    inputProduto.focus()
+    let textProduto = document.createElement('th')
+    let textCodigo = document.createElement('th')
+    let textQuant = document.createElement('th')
+    textProduto.textContent = 'Produto'
+    textCodigo.textContent = 'Código'
+    textQuant.textContent = 'Quantidade'
+    let tr = document.createElement('tr')
+    tr.className = 'flex justify-around border-be-2 p-1'
+    tr.appendChild(textCodigo)
+    tr.appendChild(textProduto)
+    tr.appendChild(textQuant)
+    produtosContainer.appendChild(tr)
+
+
+    
+    inputCodigo.focus()
 }
